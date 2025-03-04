ae89a7eef32537c01900be387123963e1456f131
piyushappinessworld <piyush.a@appinessworld.com>
2025-03-04 14:19:18 +0530

    contact us and projects  page completed

--- components/HomePage/Solutions/Solutions.jsx
+++ components/HomePage/Solutions/Solutions.jsx
@@ -1,19 +1,43 @@
+"use client"
 import React from "react";
-import {
-  ClipboardList,
-  Building2,
-  Calculator,
-  Clock,
-  Building,
-  FileSpreadsheet,
-} from "lucide-react";
 import Image from "next/image";
 
 const Solutions = () => {
+  const sol = [
+    {
+      title: "Planning",
+      img1: "/banner/sol1.svg",
+      img2: "/banner/s1.svg",
+    },
+    {
+      title: "Feasibility Studies",
+      img1: "/banner/s2.svg",
+      img2: "/banner/sol4.svg",
+    },
+    {
+      title: "Cost Estimation",
+      img1: "/banner/s3.svg",
+      img2: "/banner/sol5.svg",
+    },
+    {
+      title: "Cost Estimation",
+      img1: "/banner/s4.svg",
+      img2: "/banner/sol2.svg",
+    },
+    {
+      title: "Construction Administration",
+      img1: "/banner/s5.svg",
+      img2: "/banner/sol3.svg",
+    },
+    {
+      title: "Project Execution",
+      img1: "/banner/s6.svg",
+      img2: "/banner/sol6.svg",
+    },
+  ];
   return (
     <section className="p-[40px] md:p-[80px] bg-[#D0A88A]">
       <div className="container mx-auto px-6 md:px-8">
-        {/* Section Header */}
         <div className="text-center mb-12 md:mb-16">
           <h2 className="text-[28px] md:text-[46px] font-semibold text-[#5E362E] mb-3">
             Tailored Solutions for Every Space
@@ -23,89 +47,26 @@ const Solutions = () => {
           </p>
         </div>
 
-        <div className="w-full  flex flex-wrap gap-[22px] lg:gap-x-[66px] lg:gap-y-[45px] items-center justify-center">
-          {/* Planning */}
-          <div className="w-[270px] h-[132px] bg-[#CE8943] border-1 border-black rounded-[8px] px-[10px] py-[16px] flex flex-col justify-center items-center gap-2">
-            <div className="flex items-center justify-center">
-              <Image
-                src="/banner/solution.svg"
-                alt="logo"
-                width={54}
-                height={54}
-              />
-            </div>
-            <h3 className="text-white text-[16px] font-medium">Planning</h3>
-          </div>
-
-          <div className="w-[270px] h-[132px] bg-white border-1 border-black rounded-[8px] px-[10px] py-[16px] flex flex-col justify-center items-center gap-2">
-            <div className="flex items-center justify-center">
-              <Image
-                src="/banner/solution (1).svg"
-                alt="logo"
-                width={54}
-                height={54}
-              />
-            </div>
-            <h3 className="text-[#242424] text-[16px] font-medium">
-              Feasibility Studies
-            </h3>
-          </div>
-
-          <div className="w-[270px] h-[132px] bg-white border-1 border-black rounded-[8px] px-[10px] py-[16px] flex flex-col justify-center items-center gap-2">
-            <div className="flex items-center justify-center">
-              <Image
-                src="/banner/solution (2).svg"
-                alt="logo"
-                width={54}
-                height={54}
-              />
-            </div>
-            <h3 className="text-[#242424] text-[16px] font-medium">
-              Cost Estimation
-            </h3>
-          </div>
-
-          <div className="w-[270px] h-[132px] bg-white border-1 border-black rounded-[8px] px-[10px] py-[16px] flex flex-col justify-center items-center gap-2">
-            <div className="flex items-center justify-center">
-              <Image
-                src="/banner/solution (3).svg"
-                alt="logo"
-                width={54}
-                height={54}
-              />
-            </div>
-            <h3 className="text-[#242424] text-[16px] font-medium">
-              Scheduling
-            </h3>
-          </div>
-
-          <div className="w-[270px] h-[132px] bg-white border-1 border-black rounded-[8px] px-[10px] py-[16px] flex flex-col justify-center items-center gap-2">
-            <div className="flex items-center justify-center">
-              <Image
-                src="/banner/solution (4).svg"
-                alt="logo"
-                width={54}
-                height={54}
-              />
-            </div>
-            <h3 className="text-[#242424] text-[16px] font-medium">
-              Construction Administration
-            </h3>
-          </div>
-
-          <div className="w-[270px] h-[132px] bg-white border-1 border-black rounded-[8px] px-[10px] py-[16px] flex flex-col justify-center items-center gap-2">
-            <div className="flex items-center justify-center">
-              <Image
-                src="/banner/solution (5).svg"
-                alt="logo"
-                width={54}
-                height={54}
-              />
-            </div>
-            <h3 className="text-[#242424] text-[16px] font-medium">
-              Project Execution
-            </h3>
-          </div>
+        <div className="w-full flex flex-wrap gap-[22px] lg:gap-x-[66px] lg:gap-y-[45px] items-center justify-center">
+          {sol?.map((item, i) => {
+            return (
+              <div
+                key={i}
+                className="w-[270px] h-[132px] hover:text-white hover:bg-[#CE8943] bg-white text-black border-1 border-black rounded-[8px] px-[10px] py-[16px] flex flex-col justify-center items-center gap-2"
+                onMouseEnter={(e) => {
+                  e.currentTarget.querySelector("img").src = item?.img2;
+                }}
+                onMouseLeave={(e) => {
+                  e.currentTarget.querySelector("img").src = item?.img1;
+                }}
+              >
+                <div className="flex items-center justify-center">
+                  <Image src={item?.img1} alt="logo" width={54} height={54} />
+                </div>
+                <h3 className="text-[16px] font-medium">{item?.title}</h3>
+              </div>
+            );
+          })}
         </div>
 
         <div className="text-center mt-12 md:mt-16">
